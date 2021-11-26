2b9a804014f308271f2663e92e805683e4bfbec1
Crit <marychernichenko@gmail.com>
2021-11-27 01:38:39 +0300

    1

--- main.py
+++ main.py
@@ -1,6 +1,66 @@
-def main():
-    print('My first git-repo')
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem
+from random import randint
+from PyQt5.QtGui import QColor, QPainter
+import sys
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(971, 710)
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        MainWindow.setFont(font)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(20, 20, 941, 81))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 971, 31))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Draw"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.s = False
+        self.pushButton.clicked.connect(self.sr)
+
+    def paintEvent(self, event):
+        if self.s:
+            qp = QPainter()
+            qp.begin(self)
+            c = QColor("yellow")
+            qp.setBrush(c)
+            for i in range(10):
+                x, y = randint(0, 900), randint(0, 900)
+                r = randint(1, 200)
+                qp.drawEllipse(x, y, r, r)
+            qp.end()
+
+    def sr(self):
+        self.s = True
+        self.repaint()
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
