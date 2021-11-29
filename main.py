9b8160b6e96d66b6ecedf20add370562f6885f66
Crit <marychernichenko@gmail.com>
2021-11-29 20:59:05 +0300

    1

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem
-from random import randint
-from PyQt5.QtGui import QColor, QPainter
-import sys
+from PIL import Image
+import sys, sqlite3
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(971, 710)
-        font = QtGui.QFont()
-        font.setPointSize(11)
-        MainWindow.setFont(font)
+        MainWindow.resize(970, 756)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(20, 20, 941, 81))
-        self.pushButton.setObjectName("pushButton")
+        self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
+        self.tableWidget.setGeometry(QtCore.QRect(10, 120, 951, 571))
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.setRowCount(0)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 971, 31))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 970, 26))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -32,31 +30,26 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "Draw"))
 
 
 class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
+        self.con = sqlite3.connect('coffee.db')
         self.setupUi(self)
-        self.s = False
-        self.pushButton.clicked.connect(self.sr)
+        self.dialogs = []
+        self.update_result()
 
-    def paintEvent(self, event):
-        if self.s:
-            qp = QPainter()
-            qp.begin(self)
-            for i in range(10):
-                c = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
-                qp.setBrush(c)
-                x, y = randint(0, 900), randint(0, 900)
-                r = randint(1, 200)
-                qp.drawEllipse(x, y, r, r)
-            qp.end()
+    def update_result(self):
+        cur = self.con.cursor()
+        result = cur.execute("""SELECT * FROM coffee""").fetchall()
+        self.tableWidget.setRowCount(len(result))
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.titles = [description[0] for description in cur.description]
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
 
-    def sr(self):
-        self.s = True
-        self.repaint()
 
 
 if __name__ == '__main__':
