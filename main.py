46b092905fedc9787a5528d47571c34e04af031a
Rityresk <95101644+Rityresk@users.noreply.github.com>
2021-11-29 22:22:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(970, 756)
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        MainWindow.setFont(font)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.tableWidget = QtWidgets.QTableWidget(self.centralwidget)
@@ -15,6 +18,12 @@ class Ui_MainWindow(object):
         self.tableWidget.setObjectName("tableWidget")
         self.tableWidget.setColumnCount(0)
         self.tableWidget.setRowCount(0)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(20, 20, 161, 71))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(200, 20, 161, 71))
+        self.pushButton_2.setObjectName("pushButton_2")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 970, 26))
@@ -30,6 +39,86 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Добавить"))
+        self.pushButton_2.setText(_translate("MainWindow", "Изменить"))
+
+
+class New(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        MainWindow.setFont(font)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(19, 29, 621, 401))
+        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        self.label_5 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_5.setObjectName("label_5")
+        self.gridLayout.addWidget(self.label_5, 4, 0, 1, 1)
+        self.label = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.lineEdit_4 = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_4.setObjectName("lineEdit_4")
+        self.gridLayout.addWidget(self.lineEdit_4, 3, 1, 1, 1)
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.gridLayout.addWidget(self.lineEdit_3, 2, 1, 1, 1)
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.gridLayout.addWidget(self.lineEdit_2, 1, 1, 1, 1)
+        self.label_4 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_4.setObjectName("label_4")
+        self.gridLayout.addWidget(self.label_4, 3, 0, 1, 1)
+        self.label_3 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_3.setObjectName("label_3")
+        self.gridLayout.addWidget(self.label_3, 2, 0, 1, 1)
+        self.lineEdit_5 = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_5.setObjectName("lineEdit_5")
+        self.gridLayout.addWidget(self.lineEdit_5, 4, 1, 1, 1)
+        self.lineEdit = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        self.lineEdit.setObjectName("lineEdit")
+        self.gridLayout.addWidget(self.lineEdit, 0, 1, 1, 1)
+        self.label_2 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_2.setObjectName("label_2")
+        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
+        self.label_6 = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label_6.setObjectName("label_6")
+        self.gridLayout.addWidget(self.label_6, 5, 0, 1, 1)
+        self.lineEdit_6 = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        self.lineEdit_6.setObjectName("lineEdit_6")
+        self.gridLayout.addWidget(self.lineEdit_6, 5, 1, 1, 1)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(360, 447, 271, 51))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 29))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label_5.setText(_translate("MainWindow", "Цена"))
+        self.label.setText(_translate("MainWindow", "Название"))
+        self.label_4.setText(_translate("MainWindow", "Вкус"))
+        self.label_3.setText(_translate("MainWindow", "Молотый/в зёрнах"))
+        self.label_2.setText(_translate("MainWindow", "Степень обжарки"))
+        self.label_6.setText(_translate("MainWindow", "Объём упаковки"))
+        self.pushButton.setText(_translate("MainWindow", "Добавить"))
 
 
 class MyWidget(QMainWindow, Ui_MainWindow):
@@ -39,6 +128,8 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.dialogs = []
         self.update_result()
+        self.pushButton.clicked.connect(self.new)
+        self.pushButton_2.clicked.connect(self.change_1)
 
     def update_result(self):
         cur = self.con.cursor()
@@ -49,6 +140,68 @@ class MyWidget(QMainWindow, Ui_MainWindow):
         for i, elem in enumerate(result):
             for j, val in enumerate(elem):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+
+    def new(self):
+        h = Appd()
+        self.dialogs.append(h)
+        h.show()
+        self.update_result()
+
+    def change_1(self):
+        h = Genre()
+        self.dialogs.append(h)
+        h.show()
+
+class Appd(QMainWindow, New):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.trl)
+
+    def trl(self):
+        name = self.lineEdit.text()
+        degree = int(self.lineEdit_2.text())
+        state = self.lineEdit_3.text()
+        taste = self.lineEdit_4.text()
+        price = self.lineEdit_5.text()
+        V = self.lineEdit_6.text()
+        self.con = sqlite3.connect('coffee.db')
+        cur = self.con.cursor()
+        result = cur.execute("""INSERT INTO coffee (name, degree, state, taste, price, V) VALUES (?, ?, ?, ?, ?, ?)""",
+                             (name, degree, state, taste, price, V))
+        self.con.commit()
+        self.con.close()
+        global ex
+        ex.update_result()
+        self.close()
+
+
+class Genre(QMainWindow, New):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton.setText("Изменить")
+        self.pushButton.clicked.connect(self.trl)
+
+
+    def trl(self):
+        global ex
+        name = self.lineEdit.text()
+        degree = int(self.lineEdit_2.text())
+        state = self.lineEdit_3.text()
+        taste = self.lineEdit_4.text()
+        price = self.lineEdit_5.text()
+        V = self.lineEdit_6.text()
+        self.con = sqlite3.connect('coffee.db')
+        cur = self.con.cursor()
+        r = ex.tableWidget.currentRow()
+        name1 = ex.tableWidget.item(r, 0).text()
+        result = cur.execute("""UPDATE coffee SET name=?, degree=?, state=?, taste=?, price=?, V=? WHERE name=?""",
+                             (name, degree, state, taste, price, V, name1))
+        self.con.commit()
+        self.con.close()
+        ex.update_result()
+        self.close()
 
 
 
